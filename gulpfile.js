de6d60b37f310dec29219c05566921757a2de5ed
codegrabber1 <poruchenko@ukr.net>
2018-01-08 22:37:10 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const multipipe = require('multipipe');
+
+
+/*==== default ====*/
+gulp.task('default', gulp.parallel(''));
