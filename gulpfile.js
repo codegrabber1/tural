dcae0993283df68156cd2c0a07eb5eeb14592941
codegrabber1 <poruchenko@ukr.net>
2018-01-09 23:29:10 +0200

    2329_09.01

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,75 @@
 'use strict';
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const multipipe = require('multipipe');
+const gulp          = require('gulp');
+const sass          = require('gulp-sass');
+const conctCss      = require('gulp-concat-css');
+const multipipe     = require('multipipe');
+const debug         = require('gulp-debug');
+const del           = require('del');
+const newer         = require('gulp-newer');
+const autoprefixer  = require('gulp-autoprefixer');
+const remember      = require('gulp-remember');
+const notify        = require('gulp-notify');
+const pug           = require('gulp-pug');
+const bs            = require('browser-sync');
+
+/*==== server ====*/
+gulp.task('server', function() {
+    // Serve files from the root of this project
+    bs.init({
+        server: "public"
+    });
+
+    bs.watch('public/**/*.*').on('change', bs.reload);
+});
+
+/*=== sass ===*/
+gulp.task('sass', function() {
+    return multipipe(
+        gulp.src('frontend/sass/main.sass', {since: gulp.lastRun('sass')}),
+        sass().on('error', sass.logError),
+        autoprefixer(),
+        remember('sass'),
+        conctCss('style.css'),
+        gulp.dest('public')
+    ).on('error', notify.onError())
+
+});
+
+/*==== pug ====*/
+gulp.task('pug', function(){
+    return multipipe(
+        gulp.src('frontend/pages/*.pug', {since: gulp.lastRun('pug')}),
+        pug({
+            pretty: true
+        }),
+        gulp.dest('public')
+    ).on('error', notify.onError())
+});
+/*==== delete ====*/
+gulp.task('del', function(){
+    return del('public');
+});
+/*==== assets ====*/
+gulp.task('assets', function(){
+    return multipipe(
+        gulp.src('frontend/assets/**', {since: gulp.lastRun('assets')}),
+        newer('public'),
+        debug({title: 'assets'}),
+        gulp.dest('public')
+    )
+});
 
 
-/*==== default ====*/
-gulp.task('default', gulp.parallel(''));
+/*==== watch ====*/
+gulp.task('watch', function(){
+    gulp.watch('frontend/sass/*.sass', gulp.series('sass'));
+    gulp.watch('frontend/assets/**/*.*', gulp.series('assets'));
+    gulp.watch('frontend/pages/*.pug', gulp.series('pug'));
+});
+
+/*==== build ====*/
+gulp.task('build', gulp.series('del', gulp.parallel('pug', 'sass', 'assets')));
+
+/*==== DEV ====*/
+gulp.task('default', gulp.series('build', gulp.parallel('watch', 'server')));
