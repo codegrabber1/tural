29facbddf34b95b3da890d9a32dc0a6f08223590
codegrabber1 <poruchenko@ukr.net>
2018-01-10 20:12:08 +0200

    2012_1001

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const gulp          = require('gulp');
 const sass          = require('gulp-sass');
 const conctCss      = require('gulp-concat-css');
+const concat      = require('gulp-concat');
 const multipipe     = require('multipipe');
 const debug         = require('gulp-debug');
 const del           = require('del');
@@ -11,27 +12,31 @@ const autoprefixer  = require('gulp-autoprefixer');
 const remember      = require('gulp-remember');
 const notify        = require('gulp-notify');
 const pug           = require('gulp-pug');
-const bs            = require('browser-sync');
+const bs            = require('browser-sync').create();
+const sourcemaps    = require('gulp-sourcemaps');
 
 /*==== server ====*/
 gulp.task('server', function() {
     // Serve files from the root of this project
     bs.init({
-        server: "public"
+        server: "public/"
     });
-
-    bs.watch('public/**/*.*').on('change', bs.reload);
+    bs.watch('public/**').on('change', bs.reload);
 });
 
 /*=== sass ===*/
 gulp.task('sass', function() {
     return multipipe(
         gulp.src('frontend/sass/main.sass', {since: gulp.lastRun('sass')}),
-        sass().on('error', sass.logError),
+        sourcemaps.init(),
         autoprefixer(),
         remember('sass'),
-        conctCss('style.css'),
-        gulp.dest('public')
+        debug({title: 'sass'}),
+        sass().on('error', sass.logError),
+        concat('style.css'),
+        debug({title: 'sass'}),
+        sourcemaps.write('.'),
+        gulp.dest('public/')
     ).on('error', notify.onError())
 
 });
@@ -40,32 +45,34 @@ gulp.task('sass', function() {
 gulp.task('pug', function(){
     return multipipe(
         gulp.src('frontend/pages/*.pug', {since: gulp.lastRun('pug')}),
+        sourcemaps.init(),
+        remember('pug'),
         pug({
             pretty: true
         }),
-        gulp.dest('public')
+        debug({title: 'pug'}),
+        sourcemaps.write('.'),
+        gulp.dest('public/')
     ).on('error', notify.onError())
 });
 /*==== delete ====*/
 gulp.task('del', function(){
-    return del('public');
+    return del('public/');
 });
 /*==== assets ====*/
 gulp.task('assets', function(){
     return multipipe(
         gulp.src('frontend/assets/**', {since: gulp.lastRun('assets')}),
         newer('public'),
-        debug({title: 'assets'}),
-        gulp.dest('public')
+        //debug({title: 'assets'}),
+        gulp.dest('public/')
     )
 });
-
-
 /*==== watch ====*/
 gulp.task('watch', function(){
-    gulp.watch('frontend/sass/*.sass', gulp.series('sass'));
+    gulp.watch('frontend/sass/**/*.*', gulp.series('sass'));
     gulp.watch('frontend/assets/**/*.*', gulp.series('assets'));
-    gulp.watch('frontend/pages/*.pug', gulp.series('pug'));
+    gulp.watch('frontend/pages/**/*.*', gulp.series('pug'));
 });
 
 /*==== build ====*/
