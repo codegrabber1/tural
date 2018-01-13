7b87e2bfe91da2b551a9234eb3523d82a0c540da
codegrabber1 <poruchenko@ukr.net>
2018-01-13 03:14:48 +0200

    0314_1301

---
+++ frontend/assets/js/custom.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function($){
+    // top menu
+
+    $(".sf-menu").superfish({
+        delay: 200,
+        speed: "fast",
+        cssArrows: false
+    });
+    $(".sf-menu").after("<div id='my-menu'>");
+    $(".sf-menu").clone().appendTo("#my-menu");
+    $("#my-menu").find("*").attr('style', '');
+    $("#my-menu").find("ul").removeClass("sf-menu");
+    $("#my-menu").mmenu({
+        extensions : [ 'widescreen', 'theme-light', 'pageshadow', 'effect-menu-slide', 'effect-listitems-slide' ],
+        navbar: {
+            title: "Туроператор TURAL"
+        }
+    });
+    var api = $("#my-menu").data("mmenu");
+    api.bind("closed", function(){
+        $(".toggle-mnu").removeClass("on");
+    });
+
+    $(".mobile-mnu").click(function(){
+        var mmAPI = $("#my-menu").data("mmenu");
+        mmAPI.open();
+        var thiss = $(this).find(".toggle-mnu");
+        thiss.toggleClass("on");
+        $(".main-mnu").slideToggle();
+        return false;
+    });
+// end top menu
+});// end ready
